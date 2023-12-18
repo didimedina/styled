c750ca9fe388f6be7a18eefb3f8429463c675c22
Didi <hi@didimedina.com>
2023-12-18 18:11:01 +0200

    toggle is working and partially styled

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import solidJs from "@astrojs/solid-js";
 import vue from "@astrojs/vue";
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), react(), vue()]
+  integrations: [react({
+    include: ['**/*.react.@(jsx|tsx)']
+  }), solidJs({
+    include: ['**/*.solid.@(jsx|tsx)']
+  }), vue(), tailwind()]
 });
