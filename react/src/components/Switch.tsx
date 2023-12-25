e61c2c3a74c98adf051ff49e7baf6b625ac42e87
Didi <hi@didimedina.com>
2023-12-25 13:26:58 +0200

    added radix colors to taiwind config

--- react/src/components/Switch.tsx
+++ react/src/components/Switch.tsx
@@ -3,19 +3,25 @@ import { useState } from "react";
 
 export default function ExampleSwitchReact() {
   const [checked, setChecked] = useState(false);
+  const [ darkTheme, setDarkTheme ] = useState(false)
 
   return (
-    <Switch.Root
-      checked={checked}
-      onCheckedChange={(e) => setChecked(e.checked)}
-      className="group flex h-fit w-fit gap-4 items-center"
-    >
-      <Switch.Control className="bg-gray-300 data-[state=checked]:bg-orange-600 relative inline-flex h-5 w-10 flex-shrink-0 cursor-pointer rounded-md border-transparent border-[1px] transition-colors duration-1000 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2">
-        <Switch.Thumb className="translate-y-[6px] data-[state=checked]:translate-y-0 pointer-events-none inline-block h-3 w-full transform rounded-b-[5px] data-[state=checked]:rounded-b-[3px] rounded-t-[3px] data-[state=checked]:rounded-t-[5px] bg-white shadow ring-0 transition duration-200 ease-in-out"/>
-      </Switch.Control>
-      <Switch.Label className="text-gray-600 data-[state=checked]:text-gray-900 transition duration-1000 ease-in-out">
-        React
-      </Switch.Label>
-    </Switch.Root>
+    <div className={ `${darkTheme? "dark" : "light"}  h-screen w-screen flex items-center justify-center bg-slate-1` }>
+      <Switch.Root
+        checked={checked}
+        onCheckedChange={(e) => {
+          setChecked(e.checked)
+          setDarkTheme(!darkTheme)
+        }}
+        className="group flex h-fit w-fit gap-4 items-center"
+      >
+        <Switch.Control className="bg-slate-6 data-[state=checked]:bg-orange-9 relative inline-flex h-5 w-10 flex-shrink-0 cursor-pointer rounded-md border-transparent border-[1px] transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-orange-11 focus:ring-offset-2">
+          <Switch.Thumb className="translate-y-[6px] data-[state=checked]:translate-y-0 pointer-events-none inline-block h-3 w-full transform rounded-b-[5px] data-[state=checked]:rounded-b-[3px] rounded-t-[3px] data-[state=checked]:rounded-t-[5px] bg-slate-1 shadow ring-0 transition duration-200 ease-in-out dark:bg-white"/>
+        </Switch.Control>
+        <Switch.Label className="text-slate-8 data-[state=checked]:text-slate-11 transition duration-200 ease-in-out">
+          React
+        </Switch.Label>
+      </Switch.Root>
+    </div>
   );
 }
--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -5,8 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        gray: generateScale("gray"),
+        slate: generateScale("slate"),
+        orange: generateScale("orange")
+      }
+    },
   },
   plugins: [],
 }
 
+
+function generateScale(name) {
+  let scale = Array.from({ length: 12 }, (_, i) => {
+    let id = i + 1;
+    return [
+      [id, `var(--${name}-${id})`],
+      [`a${id}`, `var(--${name}-a${id})`],
+    ];
+  }).flat();
+
+  return Object.fromEntries(scale);
+}
